c5894bca3a35318de81a5fc92970504480715e9b
王伟行 <wangweihang_china@163.com>
2021-11-30 17:03:57 +0800

    Update pre_net.py

--- model/pre_net.py
+++ model/pre_net.py
@@ -79,16 +79,8 @@ class Pre_Net(nn.Module):
         x = self.bn1(x)
         x = self.relu(x)
         x = self.layer1(x)
-        # x = self.bn1(x)
-        # x = self.relu(x)
-        x1 = x.view(b, n, 64, 24, 24)
 
-        # x = self.layer2(x)
-        # x = self.layer3(x)
-        # x = self.layer4(x)
-        # x = self.avgpool(x)
-        # x = torch.flatten(x, 1)
-        # x = self.fc(x)
+        x1 = x.view(b, n, 64, 24, 24)
 
         return x1
 
