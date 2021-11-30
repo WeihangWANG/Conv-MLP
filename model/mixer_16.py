c4d2557bb613e017da2454ea027c27f5da34023e
王伟行 <wangweihang_china@163.com>
2021-11-30 17:03:24 +0800

    Update mixer_16.py

--- model/mixer_16.py
+++ model/mixer_16.py
@@ -31,16 +31,6 @@ class Mixer(nn.Module):
         self.layer3 = self._make_layer(block, 256, 3, stride=2)
         self.layer4 = self._make_layer(block, 512, 3, stride=2)
 
-        # self.post_conv2 = nn.Sequential(nn.Conv2d(128, 128, kernel_size=1),
-        #                                      nn.ReLU(inplace=True),
-        #                                      nn.BatchNorm2d(128))
-        # self.post_conv3 = nn.Sequential(nn.Conv2d(256, 256, kernel_size=1),
-        #                                 nn.ReLU(inplace=True),
-        #                                 nn.BatchNorm2d(256))
-        # self.post_conv4 = nn.Sequential(nn.Conv2d(512, 512, kernel_size=1),
-        #                                 nn.ReLU(inplace=True),
-        #                                 nn.BatchNorm2d(512))
-
         # 9 patches 1*1 conv
         self.bottleneck_1 = nn.Sequential(nn.LayerNorm([24, 24]),
             nn.Conv2d(16, 16, kernel_size=1),
@@ -82,64 +72,47 @@ class Mixer(nn.Module):
 
     def forward(self, x):
         b, n, c, w, h = x.shape
-        # print('mimxer input', x.size())
-        # cut = x.clone()
-        # cut = cut.view(b*n, 64, 24, 24)
+        
         x = x.transpose(1, 2)# print("color_fea size : ", color_fea.size())
         x = x.reshape(b * 64, n, 24, 24)
 
         x1 = self.bottleneck_1(x)
-        # x1 = x1 + x
-        # x1 = self.relu(x1)
 
         x1 = x1.view(b, 64, n, 24, 24)
         x1 = x1.transpose(1, 2)
         x1 = x1.reshape(b * n, 64, 24, 24)
 
         x1 = self.layer2(x1) #; print('e1',x.size())
-        # x1 = self.post_conv2(x1)
 
         x1 = x1.view(b, n, 128, 12, 12)
         x1 = x1.transpose(1, 2)
         x1 = x1.reshape(b * 128, n, 12, 12)
 
         x2 = self.bottleneck_2(x1) #; print('e2',x.size())
-        # x2 = x2 + x1
-        # x2 = self.relu(x2)
 
         x2 = x2.view(b, 128, n, 12, 12)
         x2 = x2.transpose(1, 2)
         x2 = x2.reshape(b * n, 128, 12, 12)
 
         x2 = self.layer3(x2)
-        # x2 = self.post_conv3(x2)
 
         x2 = x2.view(b, n, 256, 6, 6)
         x2 = x2.transpose(1, 2)
         x2 = x2.reshape(b * 256, n, 6, 6)
 
         x3 = self.bottleneck_3(x2)
-        # x3 = x3 + x2
-        # x3 = self.relu(x3)
 
         x3 = x3.view(b, 256, n, 6, 6)
         x3 = x3.transpose(1, 2)
         x3 = x3.reshape(b * n, 256, 6, 6)
 
         x3 = self.layer4(x3)
-        # x3 = self.post_conv4(x3)
 
-        # cut = self.shortcut(cut)
-        # x4 = x3 + cut
-        # x4 = self.norm(x4)
         x4 = x3.view(b, n, 512, 3, 3)
         x4 = x4.transpose(1, 2)
         x4 = x4.reshape(b * 512, n, 3, 3)
 
         x4 = self.bottleneck_4(x4)
-
-        # x4 = x4 + x3
-        # x4 = self.relu(x4)
         x4 = x4.view(b, 512, n, 3, 3)
 
         return x4
