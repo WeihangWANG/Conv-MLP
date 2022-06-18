42d538b316c8eb39d9724e7da50c717a381499bc
王伟行 <wangweihang_china@163.com>
2022-06-18 23:56:44 +0800

    Update build.py

--- RGB/models/build.py
+++ RGB/models/build.py
@@ -2,7 +2,7 @@ from .conv_mlp import ConvMLP
 
 def build_model(config):
     model_type = config.MODEL.TYPE
-    if model_type == 'swin':
+    if model_type == 'conv_mlp':
         model = SwinTransformer(
                                 num_classes=config.MODEL.NUM_CLASSES
                                 )
